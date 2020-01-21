5754c2b2bcbe3e8f6c728b63761aafa711f300b6
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-01-21 08:51:46 -0300

    arquivo de setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bagoftools-core',
+    version='0.1',
+    packages=['src'],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'nltk'
+    ],
+    entry_points='''
+        [console_scripts]
+        bagoftools=src:cli
+    ''',
+)
