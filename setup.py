6933a2557cf80a3e0fb3cd4df25ff0fba88a9e37
daniel-santos-7 <danielsantos7@outlook.com.br>
2020-03-04 16:28:38 -0300

    funções e testes básicos do tokenizador desenvolvidos.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup;
 setup(
     name='bagoftools-core',
     version='0.1',
+    author='GELC',
+    url='https://github.com/gelcc/bagoftools-core',
     packages=['src'],
     include_package_data=True,
     install_requires=[
--- src/__main__.py
+++ src/__main__.py
@@ -4,37 +4,38 @@
 # biblioteca utilizada para definir os comandos.
 import click;
 
-# função para ler um arquivo de texto.
-import src.file_system;
-
 # módulo tokenizador.
-import src.tokenizer;
+from src.tokenizer import word_tokenize, sent_tokenize;
 
 # definir grupo de comandos.
-@click.group()
+@click.group(context_settings={'help_option_names':['-h','--help']})
+@click.version_option('0.1 (Alpha)','-v','--version')
 def cli():
+    ''' 
+        GELC - Bag Of Tools Core\n
+        Project Repository: https://github.com/gelcc/bagoftools-core
+    '''
     pass;
 
 # comando para tokenizar um arquivo de texto.
-@cli.command()
-@click.argument('file_path', type=click.Path(exists=True))
-def tokenize(file_path):
+@cli.command('tokenize', short_help='Tokenize text.')
+@click.argument('file', type=click.File(encoding='utf8'))
+@click.option('-w', '--words','tokenization_type', flag_value='words', help='Tokenize words with punctuations.')
+@click.option('-s', '--sents','tokenization_type', flag_value='sentences', help='Tokenize sentences.')
+def tokenize(file, tokenization_type):
 
-    # função para receber o local do arquivo.
-    formated_file_path = click.format_filename(file_path);
+    # receber conteúdo do arquivo.
+    file_content = file.read();
 
-    # ler arquivo de texto.
-    file = read_text_file(formated_file_path);
+    # tokenizar conteúdo.
+    if(tokenization_type == 'sentences'):
+        tokens = sent_tokenize(file_content);
 
-    # receber conteúdo do arquivo de texto.
-    file_content = file['content'];
+    else:
+        tokens = word_tokenize(file_content);
 
-    # tokenizar palavras.
-    tokens = tokenize_words(file_content);
-    
-    # exibir tokens.
-    click.echo(tokens);
+    # formatar saída.
+    output = '\n'.join(tokens).encode('utf8');
 
-@cli.command()
-def hello():
-    click.echo('olá');
+    # exibir resultado;
+    click.echo(output);
